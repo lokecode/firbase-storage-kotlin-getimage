6980f044fa97c1f3174fab6e43e911f0727d4670
lokecode <zinck65@gmail.com>
2021-08-16 13:59:20 +0200

    first commit

--- app/src/main/java/com/demo/picturefirebasekotlin/MainActivity.kt
+++ app/src/main/java/com/demo/picturefirebasekotlin/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.RecyclerView
 import com.demo.picturefirebasekotlin.databinding.ActivityMainBinding
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,67 +22,45 @@ import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var filepath : Uri
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            startFileChooser()
-        }
-        button2.setOnClickListener {
-            uploadFile()
-        }
+        start()
+
+        binding.getImage.setOnClickListener {   // getiamge is a botton and binding is the layout
+
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Fetching image....")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+
+
+            val imageName = binding.etImageId.text.toString()
+            val storageRef = FirebaseStorage.getInstance().reference.child("images/$imageName.jpg")   // important
 
 
 
+            val localfile = File.createTempFile("tempImage", "jpg") // important
+            storageRef.getFile(localfile).addOnSuccessListener {
+
+                if(progressDialog.isShowing)
+                    progressDialog.dismiss()
+
+                val bitmap = BitmapFactory.decodeFile(File.createTempFile("tempImage", "jpg").absolutePath)  // important
+                binding.imageView.setImageBitmap(bitmap)   // important
 
 
-    }
+            }.addOnFailureListener {
 
-
-    private fun uploadFile() {
-        if(filepath != null) {
-            var pd = ProgressDialog(this)
-            pd.show()
-
-            var imageRef = FirebaseStorage.getInstance().reference.child("images/pic.jpg")
-            imageRef.putFile(filepath)
-                    .addOnSuccessListener {
-                        pd.dismiss()
-                        Toast.makeText(applicationContext, "File uploaded", Toast.LENGTH_LONG).show()
-
-                    }
-                    .addOnFailureListener { p0 ->
-                        pd.dismiss()
-                        Toast.makeText(applicationContext, p0.message, Toast.LENGTH_LONG).show()
-                    }
-                    .addOnProgressListener { p0 ->
-                        var progress = (100.0 * p0.bytesTransferred) / p0.totalByteCount
-                        pd.setMessage("uploaded ${progress.toInt()}%")
-                    }
-
+                if(progressDialog.isShowing)
+                    progressDialog.dismiss()
+                Toast.makeText(this, "Failed to retreave image", Toast.LENGTH_SHORT).show()
+            }
         }
     }
-
-
-
-    private fun startFileChooser() {
-        var i = Intent()
-        i.setType("image/*")
-        i.setAction(Intent.ACTION_GET_CONTENT)
-        startActivityForResult(Intent.createChooser(i, "Choose Picture"), 111)
+    fun start() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==111 && resultCode == RESULT_OK && data != null) {
-            filepath = data.data!!
-            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filepath)
-            imageView.setImageBitmap(bitmap)
-
-
-        }
-    }
-
 }
